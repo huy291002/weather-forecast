369387f1a04fcd228f6240ef3bc787f7f72c9afc
Man Huy Hong <honghuyman9102@gmail.com>
2023-04-27 13:32:06 +0700

    refactor

---
+++ db/init-mongo.sh
@@ -0,0 +1,1 @@
+mongoimport --authenticationDatabase=admin --uri='mongodb://mern:merndocker@:27017/test' --collection='data' --file=/data.csv --type=csv --fields="time","solid_humidity","air_humidity","temperature","light_intensity" --maintainInsertionOrder
--- init-mongo.sh
+++
@@ -1,8 +0,0 @@
-mongoimport \
-   --authenticationDatabase=admin \
-   --uri='mongodb://mern:merndocker@:27017/test' \
-   --collection='data' \
-   --file=/data.csv \
-   --type=csv \
-   --fields="time","solid_humidity","air_humidity","temperature","light_intensity" \
-   --maintainInsertionOrder
