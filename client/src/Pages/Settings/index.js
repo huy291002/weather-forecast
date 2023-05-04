f2f09327a47d5498d41816f1554e20f8123573ef
blackcat22521 <blackcat22521@gmail.com>
2023-05-04 18:53:43 +0700

    complete UI

--- client/src/Pages/Settings/index.js
+++ client/src/Pages/Settings/index.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './settings.css';
 
 function Settings() {
+  const [mode, setMode] = useState('light');
+
+  function toggleMode() {
+    setMode(prevMode => (prevMode === 'light' ? 'dark' : 'light'));
+    document.body.classList.toggle('dark-mode');
+  }
+
   return (
-    <div>settings</div>
+    <div className="toggle-container">
+      <span className="button-text">Switch to mode</span>
+      <Button onClick={toggleMode} className={`toggle-button ${mode}`}>
+        <FontAwesomeIcon icon={mode === 'light' ? faMoon : faSun} className="icon" />
+        <div className="swipe-indicator"></div>
+      </Button>
+      {/* Rest of your Settings component */}
+    </div>
   )
 }
 
-export default Settings
+export default Settings;
